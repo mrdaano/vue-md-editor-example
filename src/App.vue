329972f7d65c1b64dc9d2ac8669fa41c29f078b5
Daan Kloetstra <daankloetstra@hotmail.com>
2020-03-27 18:46:31 +0100

    Demo custom button support

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mt-3">
     <h1>Vue simple markdown example</h1>
-    <md-editor v-model="value"></md-editor>
+    <md-editor ref="editor" v-model="value" :custombuttons="customButtons"></md-editor>
     <h4 class="mt-4">Output from editor</h4>
     <div class="text-muted">{{ value }}</div>
   </div>
@@ -12,8 +12,21 @@ export default {
   name: "App",
   data() {
     return {
-      value: ""
+      value: "",
+      customButtons: [
+        {
+          label: "Insert a cat",
+          key: "cat",
+          icon: "fas fa-cat",
+          group: "advanced"
+        }
+      ]
     };
+  },
+  mounted() {
+    this.$refs.editor.$on("insert:cat", editor => {
+      editor.insertImage("Cat image", "http://placekitten.com/100/100");
+    });
   }
 };
 </script>
